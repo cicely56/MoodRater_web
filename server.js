59cd68c6863ff16845170356c58fcecdbc8717c9
lichkkkk <changli0914@gmail.com>
2018-11-29 23:04:34 -0500

    make proj deployable to AppEngine

--- server.js
+++ server.js
@@ -22,16 +22,16 @@ var SampleApp = function() {
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        // self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.PORT || 8080;
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-            self.port      = 8080;
-        };
+        // if (typeof self.ipaddress === "undefined") {
+        //    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+        //    //  allows us to run/test the app locally.
+        //    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+        //    self.ipaddress = "127.0.0.1";
+        //    self.port      = 8080;
+        // };
     };
 
 
@@ -168,11 +168,14 @@ var SampleApp = function() {
      *  Start the server (starts up the sample application).
      */
     self.start = function() {
+
+      self.app.listen(self.port);
+
         //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
+        // self.app.listen(self.port, self.ipaddress, function() {
+        //    console.log('%s: Node server started on %s:%d ...',
+        //                Date(Date.now() ), self.ipaddress, self.port);
+        // });
     };
 
 };   /*  Sample Application.  */
